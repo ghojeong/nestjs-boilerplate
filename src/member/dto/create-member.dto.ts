153b9cb7adedb2df765061f6ee645f26ef9ae92c
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-03-14 16:32:00 +0900

    feat: class validator 와 transformer 적용

--- src/member/dto/create-member.dto.ts
+++ src/member/dto/create-member.dto.ts
@@ -1,19 +1,27 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateMemberDto {
   @Field(() => String)
+  @IsString()
   userId: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   isPrivate: boolean;
 
   @Field(() => String)
+  @IsString()
   address: string;
 
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   firstName: string;
 
   @Field(() => String)
+  @IsString()
+  @Length(2, 8)
   lastName: string;
 }
