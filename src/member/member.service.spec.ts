9fe17f897388def25e977846486be5de64225c59
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-03-15 16:08:40 +0900

    test: member.service 의 createMember 에 관한 단위 테스트 작성

--- src/member/member.service.spec.ts
+++ src/member/member.service.spec.ts
@@ -3,40 +3,78 @@ import { MemberService } from './member.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Member } from './entity/member.entity';
 import { AuthService } from 'src/auth/auth.service';
+import { ObjectLiteral, Repository } from 'typeorm';
 
-const mockMemberRepository = {
-  findOne: jest.fn(),
-  save: jest.fn(),
-  create: jest.fn(),
-};
-
-const mockAuthService = {
-  signin: jest.fn(),
-  verify: jest.fn(),
-};
+type MockRepository<Entity extends ObjectLiteral> = Partial<
+  Record<keyof Repository<Entity>, jest.Mock>
+>;
 
 describe('UserService', () => {
-  let service: MemberService;
+  let memberService: MemberService;
+  let memberRepository: MockRepository<Member>;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [
         MemberService,
         {
           provide: getRepositoryToken(Member),
-          useValue: mockMemberRepository,
+          useValue: {
+            exists: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+          },
         },
         {
           provide: AuthService,
-          useValue: mockAuthService,
+          useValue: {
+            signin: jest.fn(),
+            verify: jest.fn(),
+          },
         },
       ],
     }).compile();
-    service = module.get<MemberService>(MemberService);
+    memberService = module.get(MemberService);
+    memberRepository = module.get(getRepositoryToken(Member));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(memberService).toBeDefined();
+  });
+
+  describe('createMember', () => {
+    const newMember = {
+      email: 'new@email.com',
+      password: 'pwd',
+    };
+    it('회원가입에 성공한다.', async () => {
+      // given: 가입되어 있지 않았다.
+      memberRepository.exists?.mockResolvedValue(false);
+
+      // when: 회원가입을 시도한다.
+      const result = await memberService.createMember(newMember);
+
+      // then: 회원가입에 성공한다.
+      expect(memberRepository.save).toHaveBeenCalledTimes(1);
+      expect(memberRepository.create).toHaveBeenCalledTimes(1);
+      expect(memberRepository.create).toHaveBeenCalledWith(newMember);
+      expect(result).toMatchObject({ ok: true });
+    });
+
+    it('이미 가입되었다면 실패한다.', async () => {
+      // given: 이미 가입이 되어있다.
+      memberRepository.exists?.mockResolvedValue(true);
+
+      // when: 회원가입을 시도한다.
+      const result = await memberService.createMember(newMember);
+
+      // then: 회원가입을 실패한다.
+      expect(result).toMatchObject({
+        ok: false,
+        error: '이미 존재하는 계정입니다.',
+      });
+    });
   });
 
   it.todo('createMember');
